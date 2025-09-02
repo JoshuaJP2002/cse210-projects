6ad3a2dcec0cda0eac4326a4563ff8bc596e5f91
Joshua Jimenez <minijoshua1711@gmail.com>
2025-09-02 17:35:57 -0400

    added a commet

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,12 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
-
+    
+        // ask the user for their name
         Console.WriteLine("What is your first name? ");
         string name = Console.ReadLine();
+        
+        // ask the user for their lastname
         Console.WriteLine("What is your last name? ");
         string lastname = Console.ReadLine();
+
         Console.WriteLine($"Your name is {lastname}, {name} {lastname}");
     }
 }
