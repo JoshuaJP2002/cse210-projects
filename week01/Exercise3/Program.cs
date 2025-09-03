8c93411306a3dc31e8171b1a6c2238384ec4cd84
Joshua Jimenez <minijoshua1711@gmail.com>
2025-09-03 00:47:20 -0400

    guess

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random rand = new Random();
+        int magic = rand.Next(1, 101);
+
+        int guess = -1;
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magic)
+                Console.WriteLine("Higher");
+            else if (guess > magic)
+                Console.WriteLine("Lower");
+            else
+                Console.WriteLine("You guessed it!");
+        }
     }
 }
