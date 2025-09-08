04967cfaa8f2c35e311ad30a651d68364bc135f4
Joshua Jimenez <minijoshua1711@gmail.com>
2025-09-08 11:59:55 -0400

    job

---
+++ week02/Resumes/job.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Job
+{
+    public string _company;
+    public string _jobTittle;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTittle} ({_company}) {_startYear}-{_endYear}");
+        
+    }
+
+}
